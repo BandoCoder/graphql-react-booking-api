60b39f256a00976257db7eab6111be7e17a5ccee
kittybagpipes <sparticus978@gmail.com>
2021-05-29 09:29:49 -0700

    basic graphql api

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+
+    }
+    type RootMutation {
+        createEvent(name: String): String
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-night coding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+  })
+);
 
 app.listen(3000);
