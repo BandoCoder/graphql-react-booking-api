1b37de7b406afd4b6707906ea64acbc48894f204
kittybagpipes <sparticus978@gmail.com>
2021-05-31 06:38:10 -0700

    graphql endpoint created with mock db

--- app.js
+++ app.js
@@ -4,29 +4,58 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+//temp for db placeholder
+const events = [];
+
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`
+    schema: buildSchema(
+      //ID is special type in graphql for storing IDS
+      //Exclimation means NOT NULL
+      `
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
 
     }
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema {
         query: RootQuery
         mutation: RootMutation
     }
-    `),
+    `
+    ),
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "Sailing", "All-night coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
